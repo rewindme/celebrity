88e6ee868306e04564a63710a2bd862c91bfa6a2
rewindme <rewind.me@me.com>
2014-07-08 01:48:17 +0900

    스크립트 추가, 서브페이지 작업 시작

--- src/js/markup.js
+++ src/js/markup.js
@@ -1,22 +1,36 @@
 $(document).ready(function(){
-	// GNB
-	$('.gnb li .gnb_menu').mouseover(function(){
+	// GNB 마우스 오버/아웃
+	$('.header').on('mouseover',".gnb_menu",function(){
 		$(this).parent().addClass('selected')
-	})
-	$('.gnb li').mouseleave(function(){
+	}).on('mouseleave',".gnb li",function(){
 		$(this).removeClass('selected')
 	})
 	
-	// 메인 목록 마우스 오버 이벤트
-	$('.celebrity_intro .thumbnail').mouseover(function(){
+	// 메인 목록 오버
+	$('.info_list .thumbnail').mouseover(function(){
 		$(this).parent().addClass('hover')
 	}).mouseout(function(){
 		$(this).parent().removeClass('hover')
 	})
 	
 	// 풋터 family Sites 이벤트 
-	$('.combobox .cb-button').click(function(){
+	$('.footer').on('click','.combobox .cb-button',function(){
 		$(this).parent().find('.cb-layer').toggle();
 	})
-	
 })
+
+// ajax include
+function load_section(param){
+	if (!param){return false;}
+	$.each(param,function(idx,value){
+		var sID = value.section, fName = value.fileName;
+		$.get("./"+fName,function(data){
+			$(sID).html($(data).find(sID).html())
+			console.log($(data).find(sID))
+		}).success(function(){
+			console.log(sID)
+		}).fail(function(){
+			console.log('못불러왔쪄욤 뿌잉뿌잉')
+		});
+	});	
+}
